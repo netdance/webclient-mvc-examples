a4f7372f291ba175e8d31ecd1b218a2768771ddb
Jim Driscoll <jamesgdriscoll@gmail.com>
2013-12-04 19:52:15 -0800

    Navigtion cleanup

--- backbone-yeoman/app/scripts/views/navigationbar.js
+++ backbone-yeoman/app/scripts/views/navigationbar.js
@@ -9,28 +9,27 @@ define([
     'use strict';
 
     var NavigationbarView = Backbone.View.extend({
-        template: JST['app/scripts/templates/navigationbar.ejs'],
         events: {
         'click .navbar-brand': 'onBrandClick',
         'click #productNavLink': 'onProductClick',
         'click #categoryNavLink': 'onCategoryClick'
-    },
-    el: '#topnav',
-    onBrandClick: function () {
-        Backbone.history.navigate('/home', {
-            trigger: true
-        });
-    },
-    onProductClick: function () {
-        Backbone.history.navigate('/products', {
-            trigger: true
-        });
-    },
-    onCategoryClick: function () {
-        Backbone.history.navigate('/categories', {
-            trigger: true
-        });
-    }
+        },
+        el: '#topnav',
+        onBrandClick: function () {
+            Backbone.history.navigate('/home', {
+                trigger: true
+            });
+        },
+        onProductClick: function () {
+            Backbone.history.navigate('/products', {
+                trigger: true
+            });
+        },
+        onCategoryClick: function () {
+            Backbone.history.navigate('/categories', {
+                trigger: true
+            });
+        }
     });
 
     return NavigationbarView;
